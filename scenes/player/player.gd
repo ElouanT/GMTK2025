661779e2824910f18ae49beaa5f2ec7f993a650d
Grandserein <grdserein@gmail.com>
2025-08-02 16:48:12 +0200

    Saut mural + boule

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 400.0
 const JUMP_VELOCITY = -800.0
+#Variable pour gérer les aut muraux et ne pas rester coller contre le mur
+var can_move := true
 
 
 func _physics_process(delta: float) -> void:
@@ -11,15 +13,34 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * 2.5 * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	if Input.is_action_just_pressed("left") or Input.is_action_just_pressed("right") or is_on_floor():
+			can_move = true
+
 	var direction := Input.get_axis("left", "right")
-	if direction:
+	if direction and can_move == true :
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	# Inertie au sol et en l'air
+		if is_on_floor():
+			velocity.x = move_toward(velocity.x, 0, 40)
+		else:
+			velocity.x = move_toward(velocity.x, 0, 7)
+	
+	# Wall jump
+	var opposite_direction := Input.get_axis("right", "left")
+	if Input.is_action_just_pressed("jump") and not is_on_floor() and is_on_wall():
+		velocity.y = JUMP_VELOCITY
+		if Input.is_action_pressed("left"):
+				velocity.x = SPEED
+				can_move = false
+		elif Input.is_action_pressed("right"):
+				velocity.x = -SPEED
+				can_move = false
+		
+		
 	move_and_slide()
